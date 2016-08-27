699eea1b13596b648a160f3b6e157b21236b9b05
Kent Fredric <kentfredric@gmail.com>
2016-08-28 03:50:55 +1200

    Initial commit

---
+++ lib/CPAN/Testers/TailLog/DB.pm
@@ -0,0 +1,29 @@
+use 5.006;    # our
+use strict;
+use warnings;
+
+package CPAN::Testers::TailLog::DB;
+
+our $VERSION  = '0.001000';
+our $DISTNAME = 'CPAN-Testers-TailLog-DB';
+
+# ABSTRACT: A persistent database for holding cpantesters stats
+
+# AUTHORITY
+
+1;
+
+=head1 NAME
+
+CPAN::Testers::TailLog::DB - A persistent database for holding cpantesters stats
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 LICENSE
+
+This software is copyright (c) 2016 by Kent Fredric.
+
+This is free software; you can redistribute it and/or modify it under the same
+terms as the Perl 5 programming language system itself.
+
